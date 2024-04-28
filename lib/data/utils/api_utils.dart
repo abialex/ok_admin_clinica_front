aa41ef1279080343a3314597e2918293fe985fef
alexis_13 <alexis.yalle.27@unsch.edu.pe>
2024-04-28 13:17:10 -0500

    fix status code

--- lib/data/utils/api_utils.dart
+++ lib/data/utils/api_utils.dart
@@ -1,13 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:either_dart/either.dart';
+import '../../infraestructura/network/http_status_codes.dart';
 import '../models/api_model/api_model.dart';
 
 class ApiUtils {
   static Either<String, T> reponseHandler<T>(
       Response<ApiModel> response, T Function(dynamic) callToJson) {
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      return Right(callToJson(response.data?.data));
+    switch (response.statusCode) {
+      case 200:
+      case 201:
+        return Right(callToJson(response.data?.data));
+      case HttpStatusCodesApp.serverConnectionError:
+        return Left(response.statusMessage ?? "Error no definido en la app");
+
+      default:
+        return Left(response.data!.message);
     }
-    return Left(response.data!.message);
   }
 }
---
+++ lib/infraestructura/network/http_status_codes.dart
@@ -0,0 +1,7 @@
+class HttpStatusCodesApp {
+  static const int ok = 200;
+  static const int created = 201;
+  static const int notFound = 404;
+  // Define tu código personalizado
+  static const int serverConnectionError = 519;
+}
