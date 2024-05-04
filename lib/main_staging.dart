2ce320738d731ae455e2f7b64a3b4ea2f7100b0f
alexis_13 <alexis.yalle.27@unsch.edu.pe>
2024-05-04 01:59:10 -0500

    aadd  packege win + bloc observer

---
+++ lib/main_staging.dart
@@ -0,0 +1,8 @@
+import 'package:admin_clinica_front/observer.dart';
+import 'ui/main.dart';
+
+void main() {
+  // AppFlavorConfig.appFlavor = Flavor.DEV;
+
+  bootstrap(() => const Main());
+}
---
+++ lib/observer.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:window_size/window_size.dart';
+// import 'package:wsapp/app/config/app_dependecy_injection.dart';
+// import 'package:wsapp/app/config/app_store_provider.dart';
+
+import 'core/di/injections.dart';
+
+class AppBlocObserver extends BlocObserver {
+  const AppBlocObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    log('onChange(${bloc.runtimeType}, $change)');
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    super.onError(bloc, error, stackTrace);
+  }
+}
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  Bloc.observer = const AppBlocObserver();
+  await setupLocator();
+  WidgetsFlutterBinding.ensureInitialized();
+  if (TargetPlatform.windows == defaultTargetPlatform || TargetPlatform.macOS == defaultTargetPlatform || TargetPlatform.linux == defaultTargetPlatform) {
+    //setWindowTitle('WS');
+    setWindowMinSize(const Size(500, 360));
+    setWindowMaxSize(Size.infinite);
+  }
+  //await Firebase.initializeApp(options: options);
+  // await AppStoreProvider.instance.initializeStore();
+
+  // Add cross-flavor configuration here
+
+  runApp(await builder());
+}
