3d8653ffea8ba57bbba50fe22cb29c80b18a0ab6
alexis_13 <alexis.yalle.27@unsch.edu.pe>
2024-05-16 23:19:55 -0500

    add assets

---
+++ lib/core/constants/app_const_images.dart
@@ -0,0 +1,6 @@
+class AppConstImages {
+  static const String _path = 'assets/images/';
+
+  static const String smLogo = '${_path}logo-sm.png';
+  static const String profile = '${_path}profile.png';
+}
