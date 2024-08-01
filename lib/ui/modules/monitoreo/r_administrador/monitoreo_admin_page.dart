83201d0b1764e46efd90770fc6cb33bdd6f9aa2a
alexis_13 <alexis.yalle.27@unsch.edu.pe>
2024-08-01 16:56:12 -0500

    avance de monitoreo

--- lib/ui/modules/monitoreo/r_administrador/monitoreo_admin_page.dart
+++ lib/ui/modules/monitoreo/r_administrador/monitoreo_admin_page.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: must_be_immutable
 
+import 'package:admin_clinica_front/core/extensions/date_time_extensions.dart';
 import 'package:admin_clinica_front/core/utils/app_colors.dart';
 import 'package:admin_clinica_front/data/models/doctor/doctor_contenedor_data_model.dart';
 import 'package:admin_clinica_front/data/models/ubicacion/ubicacion_contenedor_data_model.dart';
 import 'package:admin_clinica_front/ui/blocs/excel/excel_cubit.dart';
 import 'package:admin_clinica_front/ui/cubits/index_cubit.dart';
 import 'package:admin_clinica_front/ui/global_widget/app_box.dart';
+import 'package:admin_clinica_front/ui/global_widget/app_sliver_list.dart';
 import 'package:admin_clinica_front/ui/global_widget/app_text_style.dart';
 import 'package:admin_clinica_front/ui/global_widget/button_base/button_success.dart';
 import 'package:admin_clinica_front/ui/global_widget/checkbox/app_checkbox_list_cubit.dart';
@@ -16,9 +18,9 @@ import 'package:admin_clinica_front/ui/global_widget/page/page_base_phone.dart';
 import 'package:admin_clinica_front/ui/global_widget/page/page_mixin_base.dart';
 import 'package:admin_clinica_front/ui/modules/monitoreo/bloc/cita_list/cita_list_admin_bloc.dart';
 import 'package:admin_clinica_front/ui/modules/monitoreo/cubit/doctor_selected_cubit.dart';
-import 'package:admin_clinica_front/ui/modules/monitoreo/r_administrador/widgets/cita_card.dart';
 import 'package:admin_clinica_front/ui/view_models/cita_view/cita_view_models.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
@@ -224,37 +226,43 @@ class MonitoreoAdminPage extends StatelessWidget with ResponsiveWidgetMixin {
                       }, loading: (stt) {
                         return const Center(child: CircularProgressIndicator());
                       }, citaLoaded: (stt) {
-                        return Column(
+                        final citasIncompletasAsistenta = stt.citas
+                            .where((element) => (element.fechaConfirmacion == null && element.fechaValidacion != null) || element.fechaConfirmacion != null && element.fechaValidacion == null)
+                            .toList();
+
+                        final citasIncompletaDoctor =
+                            stt.citas.where((element) => (element.fechaInicio == null && element.fechaFin != null) || element.fechaInicio != null && element.fechaFin == null).toList();
+                        return Stack(
                           children: [
-                            Expanded(
-                              flex: 3,
-                              child: ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: stt.citas.length,
-                                itemBuilder: (context, index) {
-                                  return CitaCardTest(cita: stt.citas[index]);
-                                },
-                              ),
+                            CitaPromedioList(
+                              citas: stt.citas,
+                              request: request,
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            Positioned(
+                              top: 0,
+                              right: 0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
                                 children: [
                                   Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
                                     children: [
-                                      AppTextGlobal.labelLightText(text: "EXCEL:"),
-                                      IconButton(
-                                          onPressed: () {
-                                            context.read<ExcelCubit>().createReporteCitas(stt.citas);
-                                          },
-                                          icon: const Icon(Icons.print)),
+                                      AppTextGlobal.lightText(text: "Citas incompletas Control de Asistenta: "),
+                                      AppTextGlobal.labelLightText(text: citasIncompletasAsistenta.length.toString()),
                                     ],
                                   ),
-                                  Expanded(child: AppTextGlobal.labelLightText(text: context.watch<ExcelCubit>().state, textAlign: TextAlign.right)),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      AppTextGlobal.lightText(text: "Citas incompletas Control de Doctor: "),
+                                      AppTextGlobal.labelLightText(text: citasIncompletaDoctor.length.toString()),
+                                    ],
+                                  ),
                                 ],
                               ),
-                            ),
+                            )
                           ],
                         );
                       }, failure: (stt) {
@@ -288,4 +296,293 @@ class MonitoreoAdminPage extends StatelessWidget with ResponsiveWidgetMixin {
       body: const SizedBox.shrink(),
     );
   }
+}
+
+class CitaPromedioList extends StatelessWidget {
+  const CitaPromedioList({
+    super.key,
+    required this.citas,
+    required this.request,
+  });
+  final List<CitaViewModel> citas;
+  final CitaRequestAdminViewModel request;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+            flex: 3,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: LayoutBuilder(
+                          builder: (context, constraints) => Container(
+                              alignment: Alignment.bottomRight,
+                              color: AppColors.slg01,
+                              child: AppTextGlobal.labelMediumText(
+                                text: "3h",
+                                colorText: AppColors.white,
+                              )),
+                        ),
+                      ),
+                      // ),
+                      Expanded(
+                        child: LayoutBuilder(
+                          builder: (context, constraints) => Container(
+                              alignment: Alignment.bottomRight,
+                              color: AppColors.slg01,
+                              child: AppTextGlobal.labelMediumText(
+                                text: "2h",
+                                colorText: AppColors.white,
+                              )),
+                        ),
+                      ),
+                      // ),
+                      Expanded(
+                        child: LayoutBuilder(
+                          builder: (context, constraints) => Container(
+                              alignment: Alignment.bottomRight,
+                              color: AppColors.slg01,
+                              child: AppTextGlobal.labelMediumText(
+                                text: "1h",
+                                colorText: AppColors.white,
+                              )),
+                        ),
+                      ),
+                      Expanded(
+                        child: LayoutBuilder(
+                          builder: (context, constraints) => Container(
+                              alignment: Alignment.bottomRight,
+                              color: AppColors.slg01,
+                              child: AppTextGlobal.labelMediumText(
+                                text: "0h ",
+                                colorText: AppColors.white,
+                              )),
+                        ),
+                      ),
+                      ColoredBox(
+                        color: AppColors.blueSecondary,
+                        child: AppTextGlobal.labelSmallText(text: 'CANT. CITAS', fontSize: 11, maxLines: 2, textAlign: TextAlign.center, colorText: AppColors.white),
+                      ),
+                      Container(
+                          child: AppTextGlobal.labelMediumText(
+                        text: "DÍA",
+                        fontSize: 11,
+                        textAlign: TextAlign.center,
+                      )),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 12,
+                  child: GraficoCita(
+                    citas: citas,
+                    dateTimeList: getDatesBetween(request.fechaInicio ?? DateTime.now(), request.fechaFin ?? DateTime.now().add(5.days)),
+                  ),
+                ),
+              ],
+            )
+
+            // ListView.builder(
+            //   shrinkWrap: true,
+            //   itemCount: citas.length,
+            //   itemBuilder: (context, index) {
+            //     return CitaCardTest(cita: citas[index]);
+            //   },
+            // ),
+            ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  AppTextGlobal.labelLightText(text: "EXCEL:"),
+                  IconButton(
+                      onPressed: () {
+                        context.read<ExcelCubit>().createReporteCitas(citas);
+                      },
+                      icon: const Icon(Icons.print)),
+                ],
+              ),
+              Expanded(child: AppTextGlobal.labelLightText(text: context.watch<ExcelCubit>().state, textAlign: TextAlign.right)),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<DateTime> getDatesBetween(DateTime startDate, DateTime endDate) {
+    List<DateTime> dates = [];
+    DateTime currentDate = startDate;
+
+    while (currentDate.isBefore(endDate) || currentDate.isAtSameMomentAs(endDate)) {
+      dates.add(currentDate);
+      currentDate = currentDate.add(const Duration(days: 1));
+    }
+
+    return dates;
+  }
+}
+
+class GraficoCita extends StatelessWidget {
+  const GraficoCita({
+    super.key,
+    required this.citas,
+    required this.dateTimeList,
+  });
+  final List<CitaViewModel> citas;
+  final List<DateTime> dateTimeList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: AppSliverList<DateTime>(
+            scrollDirection: Axis.horizontal,
+            items: List.generate(dateTimeList.length, (index) => dateTimeList[index]),
+            itemBuilder: (context, item) {
+              //LOGIC
+
+              final List<CitaViewModel> citasDia = citas
+                  .where(
+                    (element) => element.fechaHoraCita.isSameDate(item) && element.fechaValidacion != null && element.fechaConfirmacion != null,
+                  )
+                  .toList();
+              double gols = citasDia.fold(
+                  0,
+                  (previousValue, element) =>
+                      previousValue +
+                      ((element.fechaValidacion?.hour ?? 0) * 60 +
+                          (element.fechaValidacion?.minute ?? 0) -
+                          (element.fechaConfirmacion?.minute ?? 0) -
+                          (element.fechaConfirmacion?.hour ?? 0) * 60)); //sacar promedio de hora la lista de citas
+              //sacar promedio de hora la lista de citas
+              if (citasDia.isNotEmpty) gols = gols / citasDia.length;
+              return GestureDetector(
+                onTap: () {
+                  if (citasDia.isEmpty) return;
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: IntrinsicHeight(
+                          child: Column(
+                            children: List.generate(
+                              citasDia.length,
+                              (index) {
+                                final cita = citasDia[index];
+                                return Row(
+                                  children: [
+                                    Text(cita.fechaValidacion?.toFormatyyyyMMddHHmmss() ?? '-'),
+                                    AppBox.w10,
+                                    Text(cita.fechaConfirmacion?.toFormatyyyyMMddHHmmss() ?? 'n'),
+                                  ],
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 2.5),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: LayoutBuilder(
+                          builder: (context, constraints) => Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              RotatedBox(
+                                quarterTurns: 1,
+                                child: AppTextGlobal.labelMediumText(
+                                  text: citasDia.isEmpty ? '' : convertDoubleToTimeString2(gols),
+                                  fontSize: 11,
+                                  maxLines: 2,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              AppBox.h6,
+                              Container(
+                                alignment: Alignment.bottomCenter,
+                                color: AppColors.blueAccent,
+                                height: constraints.maxHeight * (gols / (60 * 4)).clamp(0, 0.9),
+                                child: AppTextGlobal.labelSmallText(
+                                  text: '${citasDia.length.toString()}\ncitas',
+                                  fontSize: 11,
+                                  maxLines: 2,
+                                  textAlign: TextAlign.center,
+                                  colorText: Colors.transparent,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      if (citasDia.isNotEmpty)
+                        Container(
+                          width: 30,
+                          alignment: Alignment.center,
+                          color: AppColors.blueSecondary,
+                          child: AppTextGlobal.labelSmallText(text: citasDia.length.toString(), fontSize: 11, maxLines: 2, textAlign: TextAlign.center, colorText: AppColors.white),
+                        ),
+                      Container(
+                        color: AppColors.lightBackgroundColor,
+                        child: AppTextGlobal.labelMediumText(
+                          text: item.toFormaMMddSlash(),
+                          fontSize: 11,
+                        ),
+                      ),
+
+                      // Expanded(
+                      //   child: LayoutBuilder(
+                      //     builder: (context, constraints) => Container(
+                      //         margin: const EdgeInsets.symmetric(horizontal: 2.5),
+                      //         color: AppColors.yellowAccent,
+                      //         child: Column(
+                      //           mainAxisAlignment: MainAxisAlignment.end,
+                      //           children: [
+                      //             RotatedBox(
+                      //               quarterTurns: 1,
+                      //               child: AppTextGlobal.labelMediumText(
+                      //                 text: convertDoubleToTimeString(60.5),
+                      //                 fontSize: 11,
+                      //               ),
+                      //             ),
+                      //             AppBox.h(constraints.maxHeight * (30.5 / 60).clamp(0, 0.9))
+                      //           ],
+                      //         )),
+                      //   ),
+                      // ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+String convertDoubleToTimeString2(double timeInMinutes) {
+  int totalSeconds = (timeInMinutes * 60).round(); // Convertir minutos a segundos
+  int hours = totalSeconds ~/ 3600; // Obtener las horas
+  int minutes = (totalSeconds % 3600) ~/ 60; // Obtener los minutos restantes
+  // int seconds = totalSeconds % 60; // Obtener los segundos restantes
+  if (hours == 0) return '${minutes}m';
+  if (minutes == 0) return '${hours}h';
+  return '${hours}h ${minutes}m';
 }
